79edb78ee17f4676c1f7e75c5919fb47aefd4cbb
Fathalla Ebrahem <fathalla.ebrahem22@gmail.com>
2022-10-26 08:48:44 +0200

    edited

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World from DevOps ITI 3 month from webhook 2022 ')
+    res.send('Hello World from DevOps ITI 3 months from webhook 2022 ')
   })
   
   app.listen(port, () => {
